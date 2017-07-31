2d61c900160ccf9f85f377e00b1537f378f47b9d
Talha Ashaque <Talha Ashaque@TALHAASHAQUE-PC>
2017-08-01 00:20:54 +0500

    dynamic menu

--- MVCWebApp/VteamWork/VteamWork.Master.cs
+++ MVCWebApp/VteamWork/VteamWork.Master.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Model;
+using VteamWork.Helper;
+using Model.DataResponse;
 
 namespace VteamWork
 {
@@ -13,13 +15,66 @@ namespace VteamWork
         protected void Page_Load(object sender, EventArgs e)
         {
             VTeamWorkDB db = new VTeamWorkDB();
-         //   List<tbl_USER> users = db.tbl_USER.ToList();
-            if (Session["userinfo"]==null)
+            //   List<tbl_USER> users = db.tbl_USER.ToList();
+            if (Session["userinfo"] == null)
             {
                 Response.Redirect("~/Login.aspx");
             }
+            else
+            {
 
-          
+                lblUserName.Text = ((tbl_USER)Session["userinfo"]).LOGIN_ID.ToString();
+                lblEmail.Text = ((tbl_USER)Session["userinfo"]).EMAIL.ToString();
+
+                innerHtml.Text = dynamicMenu(((tbl_USER)Session["userinfo"]));
+            }
+
+        }
+            public string dynamicMenu(tbl_USER user)
+        {
+            string txtHTML = "";
+
+            Response resp = LoginHelper.GetMenuList(user);
+            if(resp.IsError==false)
+            { 
+            List<tbl_MENU_GROUP> menuList = (List<tbl_MENU_GROUP>)resp.data;
+                if (menuList.Count > 0)
+                {
+                    txtHTML += @"<li>";
+                    foreach (var lst in menuList)
+                    {
+                       
+                        if (lst.PARENT_MENU_GROUP_ID == 0)
+                        {
+                            txtHTML += " <a href = \"javascript:void(0);\" class=\"menu-toggle\"><span>" + lst.MENU_GROUP_NAME + "</span></a> ";
+                        }
+                        else
+                        {
+
+                             resp = LoginHelper.GetPageList((int)lst.MENU_GROUP_ID);
+                            if (resp.IsError == false)
+                            {
+                                List<tbl_PAGE> pageList = (List<tbl_PAGE>)resp.data;
+                                if(pageList.Count>0)
+                                {
+                                    txtHTML += "<ul class=\"ml-menu\"><li>";
+                                    txtHTML += " <a href = \"javascript:void(0);\" class=\"menu-toggle\"><span>" + lst.MENU_GROUP_NAME + "</span></a> ";
+                                    txtHTML += "<ul class=\"ml-menu\">";
+                                    foreach (var lst2 in pageList)
+                                    {
+                                        txtHTML += "<li><a href = \"" + lst2.PAGE_PATH + "\" > " + lst2.PAGE_NAME + "</a></ li >";
+                                     }
+                                    txtHTML += "</ul>";
+                                    txtHTML += "</li></ul>";
+                                }
+                            }
+                        }
+                    }
+                    txtHTML += @"</li>";
+                }
+            }
+
+            return txtHTML;
         }
     }
 }

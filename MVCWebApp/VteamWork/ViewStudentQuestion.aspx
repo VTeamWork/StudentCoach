9638688c3353ced32e3381850fdb56a974ca46d5
ateebbaqai07 <30473387+ateebbaqai07@users.noreply.github.com>
2017-09-07 21:37:28 -0700

    work in progress

---
+++ MVCWebApp/VteamWork/ViewStudentQuestion.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/VteamWork.Master" AutoEventWireup="true" CodeBehind="ViewStudentQuestion.aspx.cs" Inherits="VteamWork.ViewStudentQuestion" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+  
+<div ID="DataShow" runat="server">
+
+
+</div>    
+
+</asp:Content>
+
+
---
+++ MVCWebApp/VteamWork/ViewStudentQuestion.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VteamWork.Helper;
+
+namespace VteamWork
+{
+
+
+
+    public partial class ViewStudentQuestion : System.Web.UI.Page
+    {
+       
+
+        public List<Model.tbl_MODULE> GetViewStudent()
+        {
+            return LoginHelper.db.tbl_MODULE.ToList();
+            //return LoginHelper.db.tbl_TEAM_MODULE.AsQueryable();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<Model.tbl_MODULE> list = GetViewStudent();
+
+            foreach (var mainmodule in list)
+            {
+                Label maincontrol = new Label() { Text = mainmodule.MODULE_NAME };
+                DataShow.Controls.Add(maincontrol);
+                foreach (var item in mainmodule.tbl_MODULE1)
+                {
+                    maincontrol.Controls.Add(new Label() { Text = item.MODULE_NAME });
+                }
+
+            }
+        }
+    }
+}
